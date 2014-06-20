5e3b76f770ac7ac5c6f2286804aa84a3040ae6cb
Vaio <vaio@vaio-VPCEH30EL.(none)>
2014-06-20 09:22:17 -0300

    Add ".metadata/" and "~" to gitignore testing commit from alejandrocastrounqui to git@github.com:linthar/ttacapi.git

--- src/main/java/ar/edu/unq/tac/market/service/exception/UserAlreadyExistsException.java
+++ src/main/java/ar/edu/unq/tac/market/service/exception/UserAlreadyExistsException.java
@@ -2,7 +2,9 @@ package ar.edu.unq.tac.market.service.exception;
 
 public class UserAlreadyExistsException extends RuntimeException {
 
-    public UserAlreadyExistsException(final String message) {
+	private static final long serialVersionUID = 1L;
+
+	public UserAlreadyExistsException(final String message) {
         super(message);
     }
 }
